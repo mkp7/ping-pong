34c43cc8c8492fa7721a5cb7c583b75eca3b4f9f
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-03-20 22:23:32 +0530

    Refactor broadcast event emmiting

--- app.js
+++ app.js
@@ -14,55 +14,32 @@ const game = {
   p2X: 640 / 2
 }
 
-let id = 0
-let conn = []
-
 io.on('connection', function (socket) {
-  socket.id = id
-  id++
-  conn[id] = socket
   console.log('a user connected')
 
   socket.on('disconnect', function () {
-    conn[socket.id] = null
     console.log('user disconnected')
   })
 
   socket.on('keyPressed', function (action) {
     if (action === 'P1LEFT') {
       game.p1X -= 20
-      conn.forEach(s => {
-        if (s !== null) {
-          s.emit('P1', game.p1X)
-        }
-      })
+      io.emit('P1', game.p1X)
     }
 
     if (action === 'P1RIGHT') {
       game.p1X += 20
-      conn.forEach(s => {
-        if (s !== null) {
-          s.emit('P1', game.p1X)
-        }
-      })
+      io.emit('P1', game.p1X)
     }
 
     if (action === 'P2LEFT') {
       game.p2X -= 20
-      conn.forEach(s => {
-        if (s !== null) {
-          s.emit('P2', game.p1X)
-        }
-      })
+      io.emit('P2', game.p2X)
     }
 
     if (action === 'P2RIGHT') {
       game.p2X += 20
-      conn.forEach(s => {
-        if (s !== null) {
-          s.emit('P2', game.p1X)
-        }
-      })
+      io.emit('P2', game.p2X)
     }
 
     console.log(game)
--- public/app.js
+++ public/app.js
@@ -1,50 +1,50 @@
 const socket = io()
-let started = false;
-let paddleWidth = 100;
-let paddleHeight = 16;
+let started = false
+let paddleWidth = 100
+let paddleHeight = 16
 
 const p1 = {}
 const p2 = {}
 
-function setup() {
-  createCanvas(640, 480);
+function setup () {
+  createCanvas(640, 480)
 }
 
-function draw() {
-  background(044);
+function draw () {
+  background(44)
   if (!started) {
-    p1.xPaddle = 640 / 2;
-    p1.yPaddle = 480 - 50;
-    p2.xPaddle = 640 / 2;
-    p2.yPaddle = 50;
-    started = true;
+    p1.xPaddle = 640 / 2
+    p1.yPaddle = 480 - 50
+    p2.xPaddle = 640 / 2
+    p2.yPaddle = 50
+    started = true
   }
 
-  fill(0, 255, 255);
-  noStroke();
-  rect(p1.xPaddle, p1.yPaddle, paddleWidth, paddleHeight);
-  rect(p2.xPaddle, p2.yPaddle, paddleWidth, paddleHeight);
+  fill(0, 255, 255)
+  noStroke()
+  rect(p1.xPaddle, p1.yPaddle, paddleWidth, paddleHeight)
+  rect(p2.xPaddle, p2.yPaddle, paddleWidth, paddleHeight)
 }
 
-function keyPressed() {
+function keyPressed () {
   if (keyCode === LEFT_ARROW) {
     // p1.xPaddle -= 30;
-    socket.emit('keyPressed', 'P1LEFT');
+    socket.emit('keyPressed', 'P1LEFT')
   }
-  
+
   if (keyCode === RIGHT_ARROW) {
     // p1.xPaddle += 30;
-    socket.emit('keyPressed', 'P1RIGHT');
+    socket.emit('keyPressed', 'P1RIGHT')
   }
 
   if (keyCode === 65) {
     // p2.xPaddle -= 30;
-    socket.emit('keyPressed', 'P2LEFT');
+    socket.emit('keyPressed', 'P2LEFT')
   }
-  
+
   if (keyCode === 68) {
     // p2.xPaddle += 30;
-    socket.emit('keyPressed', 'P2RIGHT');
+    socket.emit('keyPressed', 'P2RIGHT')
   }
 }
 
